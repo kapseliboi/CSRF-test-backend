b91226a72d3cd03a098c74d1aae536198a614fd5
Akseli Käppi <akseli.kappi@gmail.com>
2020-04-03 21:52:12 +0300

    Create a constant enum for strings used in responses

---
+++ src/constants/strings.ts
@@ -0,0 +1,7 @@
+export const enum ResponseStrings {
+    internalSomethingWentWrong = 'Something went wrong. Please try again.',
+    accountEmailIsNotVerified = 'Account email hasn\'t been verified. Check your mailbox.',
+    accountDisabled = 'Account is disabled. There\'s nothing you can do.',
+    credentialsIncorrect = 'Username, email or password incorrect.',
+    unableToSendVerificationEmail = 'Server was unable to send the verification email. Please contact support.',
+};
