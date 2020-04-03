39cde6230dadea499191caf559230a31e88fe442
Akseli Käppi <akseli.kappi@gmail.com>
2020-04-03 21:59:13 +0300

    Add new secrets

--- src/config.ts
+++ src/config.ts
@@ -20,8 +20,18 @@ function stringToBool(value: string): boolean {
 
 export default {
     NODE_ENV: optionalEnv('NODE_ENV'),
-    SECRET_KEY: mandatoryEnv('SECRET_KEY'),
+    JWT_SECRET_KEY: mandatoryEnv('JWT_SECRET_KEY'),
+    EMAIL_TOKEN_SECRET_KEY: mandatoryEnv('EMAIL_TOKEN_SECRET_KEY'),
+    FRONTEND_URL: mandatoryEnv('FRONTEND_URL'),
     HTTPS_ONLY: stringToBool(optionalEnv('HTTPS_ONLY')),
     PORT: optionalEnv('PORT') || 8080,
     DATABASE_URL: mandatoryEnv('DATABASE_URL'),
+    // Mailgun config, required for sending email
+    MAILGUN_API_KEY: optionalEnv('MAILGUN_API_KEY'),
+    MAILGUN_DOMAIN: optionalEnv('MAILGUN_DOMAIN'),
+    MAILGUN_PUBLIC_KEY: optionalEnv('MAILGUN_PUBLIC_KEY'),
+    MAILGUN_SMTP_LOGIN: optionalEnv('MAILGUN_SMTP_LOGIN'),
+    MAILGUN_SMTP_PASSWORD: optionalEnv('MAILGUN_SMTP_PASSWORD'),
+    MAILGUN_SMTP_PORT: optionalEnv('MAILGUN_SMTP_PORT'),
+    MAILGUN_SMTP_SERVER: optionalEnv('MAILGUN_SMTP_SERVER')
 };
