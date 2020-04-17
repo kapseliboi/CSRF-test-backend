8cc1b0371441f91385564400353a32512cf4e94b
Akseli Käppi <akseli.kappi@gmail.com>
2020-04-17 17:26:36 +0300

    Add model for a post

---
+++ src/entity/Post.ts
@@ -0,0 +1,36 @@
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    CreateDateColumn,
+    ManyToOne,
+    UpdateDateColumn,
+    JoinColumn,
+} from 'typeorm';
+import { User } from './User';
+
+@Entity()
+export class Post {
+
+    @PrimaryGeneratedColumn('increment')
+    id: number;
+
+    @Column()
+    userId: number;
+
+    @ManyToOne(type => User, user => user.posts)
+    @JoinColumn({ name: 'userId' })
+    user: User;
+
+    @Column('text')
+    title: string;
+
+    @Column('text')
+    text: string;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    editedAt: Date;
+}
