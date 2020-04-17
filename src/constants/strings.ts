6590fab650ae69ef66dd779c59f25fc0d21d6f25
Akseli Käppi <akseli.kappi@gmail.com>
2020-04-17 17:26:12 +0300

    Add new error strings

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -4,4 +4,6 @@ export const enum ResponseStrings {
     accountDisabled = 'Account is disabled. There\'s nothing you can do.',
     credentialsIncorrect = 'Username, email or password incorrect.',
     unableToSendVerificationEmail = 'Server was unable to send the verification email. Please contact support.',
+    postNotFound = 'No posts found with the provided id.',
+    notAuhtorized = 'You are not authorized to complete this action.',
 };
