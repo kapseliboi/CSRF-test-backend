e57728830f1e39f65968ea70e05021d802272cf7
Akseli Käppi <akseli.kappi@gmail.com>
2020-04-17 17:35:16 +0300

    Add typings for requests
    This removes the need for casting

--- src/types/requests.ts
+++ src/types/requests.ts
@@ -1,4 +1,5 @@
-import { Request } from '@hapi/hapi';
+import { Request, RequestAuth } from '@hapi/hapi';
+import { JWTToken } from '../util/authentication';
 
 export interface RegistrationRequest extends Request {
     payload: {
@@ -8,11 +9,11 @@ export interface RegistrationRequest extends Request {
     };
 };
 
-export interface EmailVerificationRequest extends Request {
+export interface EmailVerificationRequest extends Omit<Request, 'params'> {
     params: {
-        id: string;
+        id: number;
         token: string;
-    };
+    }
 };
 
 export interface LoginRequest extends Request {
@@ -21,3 +22,34 @@ export interface LoginRequest extends Request {
         password: string;
     }
 }
+
+export interface AuthenticatedRequest extends Omit<Request, 'auth'> {
+    auth: JWTRequestAuth;
+};
+
+interface JWTRequestAuth extends Omit<RequestAuth, 'credentials'> {
+    credentials: JWTToken;
+};
+
+export interface GetOrDeletePostRequest extends Omit<AuthenticatedRequest, 'params'> {
+    params: {
+        id: number;
+    };
+};
+
+export interface CreatePostRequest extends AuthenticatedRequest {
+    payload: {
+        title: string;
+        text: string;
+    };
+};
+
+export interface EditPostRequest extends Omit<AuthenticatedRequest, 'params'> {
+    payload: {
+        title: string;
+        text: string;
+    };
+    params: {
+        id: number;
+    };
+};
