d0932491c23e5cef870af5707fce6c8386af76e1
Akseli Käppi <akseli.kappi@gmail.com>
2020-04-17 17:32:23 +0300

    Add relation to post

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,4 +1,11 @@
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn } from 'typeorm';
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    CreateDateColumn,
+    OneToMany,
+} from 'typeorm';
+import { Post } from './Post';
 
 @Entity()
 export class User {
@@ -37,4 +44,7 @@ export class User {
 
     @CreateDateColumn()
     createdAt: Date;
+
+    @OneToMany(type => Post, post => post.user)
+    posts: Post[];
 }
