79048d6162210266678f99a004c913ca17470b6c
Akseli Käppi <akseli.kappi@gmail.com>
2020-04-17 17:36:27 +0300

    Add post-repository

--- src/repositories.ts
+++ src/repositories.ts
@@ -1,7 +1,9 @@
 import { getConnection } from 'typeorm';
 import { User } from './entity/User';
 import { FakeUser } from './entity/FakeUser';
+import { Post } from './entity/Post';
 
 const connection = getConnection();
 export const userRepository = connection.getRepository(User);
 export const fakeUserRepository = connection.getRepository(FakeUser);
+export const postRepository = connection.getRepository(Post);
