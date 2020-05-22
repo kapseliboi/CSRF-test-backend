81bb9df03f5a7fd2bb35f46fce8a2b0ab184e6c1
Akseli Käppi <akseli.kappi@gmail.com>
2020-05-22 21:54:24 +0300

    Fix heroku build errors

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 npm run build-prod
 git clone https://github.com/kapseliboi/CSRF-frontend-test.git frontend
 cd frontend
-npm install
+npm install --only=dev && npm install
 npm run build
 cp -r build ../dist/frontend
